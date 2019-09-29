2aa7af74d1d813285b640dd3b15bd66e50d0c598
ktscates <cates2506@gmail.com>
2019-09-29 21:55:08 +0200

    Change DB connection

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -4,5 +4,7 @@ import org.sql2o.*;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news", "ktscates", "ktscates");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news", "ktscates", "ktscates");
+String connectionString = "jdbc:postgresql://ec2-174-129-227-80.compute-1.amazonaws.com:5432/dbpp7ee2scfmrg"; //!
+    Sql2o sql2o = new Sql2o(connectionString, "gwxlldtvqdqxek", "e500d9c332d9a0119ee3f0119f13671bfa9117822ec9cc3fb38e06f1e3e270ca"); //!
 }
