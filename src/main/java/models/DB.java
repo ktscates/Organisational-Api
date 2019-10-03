2b663e87800108441be961d1d400bc2fce5b481b
ktscates <cates2506@gmail.com>
2019-10-03 11:13:25 +0200

    Minor changes

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,10 +1,36 @@
-package models;
-
-import org.sql2o.*;
+        package models;
+        import org.sql2o.*;
+        import java.net.URI;
+        import java.net.URISyntaxException;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
 
 public class DB {
+    private static URI dbUri;
+    public static Sql2o sql2o;
 
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news", "ktscates", "ktscates");
-    public static String connectionString = "jdbc:postgresql://ec2-174-129-43-40.compute-1.amazonaws.com:5432//d2ig9l20sv4fei"; //!
-    public static Sql2o sql2o = new Sql2o(connectionString, "whkxlzdymvmkhf", "34d10f3172ad1e41df97db4247ff789b6d1ab67b50f570d0158d733c272f7b08"); //!
+    static {
+        Logger logger = LoggerFactory.getLogger(DB.class);
+
+        try {
+
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+//            if (System.getenv("DATABASE_URL") == null) {
+//                dbUri = new URI("postgres://localhost:5432/news");
+//                logger.info("Using local database.");
+//            } else {
+//                dbUri = new URI(System.getenv("DATABASE_URL"));
+//            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "ktscates" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "ktscates" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
