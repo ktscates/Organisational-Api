ff4c8ae22b3ac313ec66625a650800a567c5950a
ktscates <cates2506@gmail.com>
2019-10-03 09:48:41 +0200

    Minor changes

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -4,7 +4,7 @@ import org.sql2o.*;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news", "ktscates", "ktscates");
-//    public static String connectionString = "jdbc:postgresql://ec2-174-129-227-80.compute-1.amazonaws.com:5432/dbpp7ee2scfmrg"; //!
-//    public static Sql2o sql2o = new Sql2o(connectionString, "gwxlldtvqdqxek", "e500d9c332d9a0119ee3f0119f13671bfa9117822ec9cc3fb38e06f1e3e270ca"); //!
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news", "ktscates", "ktscates");
+    public static String connectionString = "jdbc:postgresql://ec2-174-129-43-40.compute-1.amazonaws.com:5432//d2ig9l20sv4fei"; //!
+    public static Sql2o sql2o = new Sql2o(connectionString, "whkxlzdymvmkhf", "34d10f3172ad1e41df97db4247ff789b6d1ab67b50f570d0158d733c272f7b08"); //!
 }
